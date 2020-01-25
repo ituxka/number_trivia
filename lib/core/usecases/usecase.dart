8676acee078dc25bbfe8416fa25f11af55dd667f
Nikita Sologub <ituxka@gmail.com>
2020-01-25 17:09:47 +0300

    feat: add get_random_number_trivia usecase and tests

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,6 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:number_trivia/core/error/failures.dart';
 
 abstract class UseCase<T, Params> {
   Future<Either<Failure, T>> call(Params params);
 }
+
+class NoParams extends Equatable {}
---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  GetRandomNumberTrivia(this.repository);
+
+  final NumberTriviaRepository repository;
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(params) {
+    return repository.getRandomNumberTrivia();
+  }
+}
