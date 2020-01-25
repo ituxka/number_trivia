73a3359d4ecae4ca2872618773f2cc0d03fdd06b
Nikita Sologub <ituxka@gmail.com>
2020-01-25 22:15:02 +0300

    feat: add number trivia repository, data source contracts. setup test for them

---
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+
+class MockRemoteDataSource extends Mock implements NumberTriviaRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  MockRemoteDataSource remoteDataSource;
+  MockLocalDataSource localDataSource;
+  MockNetworkInfo networkInfo;
+
+  setUp(() {
+    remoteDataSource = MockRemoteDataSource();
+    localDataSource = MockLocalDataSource();
+    networkInfo = MockNetworkInfo();
+    repository = NumberTriviaRepositoryImpl(
+      remoteDataSource: remoteDataSource,
+      localDataSource: localDataSource,
+      networkInfo: networkInfo,
+    );
+  });
+}
