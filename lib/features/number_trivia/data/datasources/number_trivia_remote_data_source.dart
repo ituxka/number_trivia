4f4e4adafc19d4f561e76847265c549b31c49449
Nikita Sologub <ituxka@gmail.com>
2020-01-26 20:53:25 +0300

    feat: implement number_trivia_remote_data_source with http withs tests

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
 import 'package:number_trivia/core/error/exceptions.dart';
+import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRemoteDataSource {
@@ -11,4 +16,34 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTrivia> getRandomNumberTrivia();
+}
+
+const API_ENDPOINT = 'http://numbersapi.com';
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  NumberTriviaRemoteDataSourceImpl({
+    @required this.client,
+  });
+
+  final Client client;
+
+  @override
+  Future<NumberTrivia> getConcreteNumberTrivia(int number) async {
+    return _getTriviaFromUrl('$API_ENDPOINT/$number');
+  }
+
+  @override
+  Future<NumberTrivia> getRandomNumberTrivia() {
+    return _getTriviaFromUrl('$API_ENDPOINT/random');
+  }
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(url, headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode != 200) {
+      throw ServerException();
+    }
+
+    return NumberTriviaModel.fromJson(json.decode(response.body));
+  }
 }
---
+++ test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:matcher/matcher.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/error/exceptions.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements Client {}
+
+void main() {
+  NumberTriviaRemoteDataSourceImpl remoteDataSource;
+  MockHttpClient httpClient;
+
+  setUp(() {
+    httpClient = MockHttpClient();
+    remoteDataSource = NumberTriviaRemoteDataSourceImpl(client: httpClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    when(httpClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => Response(fixture('trivia.json'), 200));
+  }
+
+  void setUpMockHttpClientError404() {
+    when(httpClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => Response('Something went wrong', 404));
+  }
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(
+      json.decode(fixture('trivia.json')),
+    );
+
+    test('should perform a GET request on a URL with number and with application/json header', () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+
+      // act
+      remoteDataSource.getConcreteNumberTrivia(tNumber);
+
+      // assert
+      verify(httpClient.get(
+        'http://numbersapi.com/$tNumber',
+        headers: {'Content-Type': 'application/json'},
+      ));
+    });
+
+    test('should return NumberTrivia when the responce code is 200 (success)', () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+
+      // act
+      final result = await remoteDataSource.getConcreteNumberTrivia(tNumber);
+
+      // assert
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('should throw a ServerException when the responce code is 404 or other', () async {
+      // arrange
+      setUpMockHttpClientError404();
+
+      // act
+      final call = remoteDataSource.getConcreteNumberTrivia;
+
+      // assert
+      expect(() => call(tNumber), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('getRandomNumberTrivia', () {
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(
+      json.decode(fixture('trivia.json')),
+    );
+
+    test('should perform a GET request on a URL with number and with application/json header', () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+
+      // act
+      remoteDataSource.getRandomNumberTrivia();
+
+      // assert
+      verify(httpClient.get(
+        'http://numbersapi.com/random',
+        headers: {'Content-Type': 'application/json'},
+      ));
+    });
+
+    test('should return NumberTrivia when the responce code is 200 (success)', () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+
+      // act
+      final result = await remoteDataSource.getRandomNumberTrivia();
+
+      // assert
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('should throw a ServerException when the responce code is 404 or other', () async {
+      // arrange
+      setUpMockHttpClientError404();
+
+      // act
+      final call = remoteDataSource.getRandomNumberTrivia;
+
+      // assert
+      expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+}
